5b63aadcea85973f4af814ce8c56706639994c62
mayi <tala.aguirre@gmail.com>
2023-08-01 18:53:53 +0200

    Changing the designs pictures paths from the src to public folder.

--- src/data/designsData.js
+++ src/data/designsData.js
@@ -2,7 +2,7 @@ export const designsData = [
     {
         id: 1,
         name: 'Arte Miralles',
-        imageSrc: './src/assets/designs/artemiralles.jpg',
+        imageSrc: './assets/designs/artemiralles.jpg',
         imageAlt: "A visual for Arte Miralles website, a sculpture studio. By Tala Aguirre",
         description: 'A hero image of the wordPress website I have built for the spanish sculptor Miralles about his work in his studio Arte Miralles.',
         design: 'The visual and the website layout have been created with Adobe Photoshop. Miralles sculpture has been shot in Studio T.A.',
@@ -10,7 +10,7 @@ export const designsData = [
       {
         id: 2,
         name: 'Arte Miralles\' logo',
-        imageSrc: './src/assets/designs/logo_miralles.jpg',
+        imageSrc: './assets/designs/logo_miralles.jpg',
         imageAlt: "Arte Miralles sculpture studio logo.",
         description: 'This logo may be used in white or black version. For little sizes requirements only the figure represents the studio.',
         design: 'The logo has been drawn with Adobe Illustrator software.',
@@ -18,7 +18,7 @@ export const designsData = [
       {
         id: 3,
         name: 'Dionysos\' logo',
-        imageSrc: './src/assets/designs/logo_dionysos.jpg',
+        imageSrc: './assets/designs/logo_dionysos.jpg',
         imageAlt: "This is a vectoriel logo designed for a wine store who sell and organize events.",
         description: 'Dionysos is a company who sell wine and organize events. This logo has been designed to be responsive and may be used in any colour .',
         design: 'The logo has been drawn with Adobe Illustrator software.',
@@ -26,7 +26,7 @@ export const designsData = [
       {
         id: 4,
         name: 'Mus game cards',
-        imageSrc: './src/assets/designs/mus_card.jpg',
+        imageSrc: './assets/designs/mus_card.jpg',
         imageAlt: "Back of a Mus game card for the Muséee basque et de l\'Histoire de Bayonne.",
         description: 'This design has been created for the Musée basque et de l\'Histoire de Bayonne and it\'s on sale in the museum shop.',
         design: 'The logo is built with Adobe Illustrator.',
@@ -34,7 +34,7 @@ export const designsData = [
       {
         id: 5,
         name: 'News Blog',
-        imageSrc: './src/assets/designs/guaido_post.jpg',
+        imageSrc: './assets/designs/guaido_post.jpg',
         imageAlt: "Visual about Juan Guaidó, the Venezuelan politician. By Tala Aguirre",
         description: 'This visual illustrate a post witch treat about Juan Guaidó, the Venezuelan politician witch has suddenly desapear from the news during the Coronavirus.',
         design: 'The visual is built with Adobe Photoshop.',
@@ -42,7 +42,7 @@ export const designsData = [
       {
         id: 6,
         name: 'Logo of Piscines et Maçonnerie',
-        imageSrc: './src/assets/designs/pool_logo.jpg',
+        imageSrc: './assets/designs/pool_logo.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         description: 'Designed for a swimming pool laborer from Dax (Fr).',
         design: 'The logo is drawn with Adobe Illustrator.',
--- src/data/verticalDesignsData.js
+++ src/data/verticalDesignsData.js
@@ -2,7 +2,7 @@ export const verticalDesignsData = [
     {
       id: 1,
       name: 'Absolut Vodka advertising',
-      imageSrc: '../src/assets/designs/absolut_pub.jpg',
+      imageSrc: './assets/designs/absolut_pub.jpg',
       imageAlt: "Illustration of Absolut Vodka for an advertising. By Tala Aguirre",
       description: 'Following the concept of the company\'s advertising, my purpose has been to tell a history about an Absolut Vodka bottle which fell into the deep water of a river by accident. It\'s just a so delicious nectar that this three gold fishes covet the drink as it was a treasure.',
       design: 'This illustration is a photomontage of 6 photographies. It has been created with Adobe Photoshop software',
